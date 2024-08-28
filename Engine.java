065e1fb3b1d1b5c6cda117954963b93d775c96f6
carsonjwood <157393231+carsonjwood@users.noreply.github.com>
2024-08-28 12:46:16 -0400

    Panel Work

--- Engine.java
+++ Engine.java
@@ -2,11 +2,14 @@
 import java.util.*;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 public class Engine {
@@ -16,11 +19,16 @@ public class Engine {
     JFrame f = new JFrame("A JFrame");
     f.setSize(500, 500);
     f.setLocation(300,200);
-    final JTextArea textArea = new JTextArea(10, 40);
-    f.getContentPane().add(BorderLayout.CENTER, textArea);
+    final JTextArea textArea = new JTextArea(1, 10);
+    f.getContentPane().add(BorderLayout.WEST, textArea);
     final JButton button = new JButton("Click Me");
     f.getContentPane().add(BorderLayout.SOUTH, button);
-    
+
+    JPanel userCharacter = new JPanel();
+    userCharacter.setSize(300,500);
+    f.getContentPane().add(BorderLayout.EAST, userCharacter);
+    userCharacter.setBackground(Color.black);
+
     button.addActionListener(new ActionListener() {
 
         @Override
