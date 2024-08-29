f32ac7565b60e9f03cbd0b5ef02b000f4a7ff261
carsonjwood <157393231+carsonjwood@users.noreply.github.com>
2024-08-29 13:35:58 -0400

    Button Setup, Engine Progression

--- Engine.java
+++ Engine.java
@@ -16,45 +16,51 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import java.awt.*;
 
+
 public class Engine {
 
-    Engine(){
+    public int storyProgress;
 
+    Engine() {
+
+        storyProgress = 1;
         JFrame f = new JFrame("A JFrame");
-    f.setSize(500, 500);
-    f.setLocation(300,200);
-    final JTextArea textArea = new JTextArea(5, 10);
-    f.getContentPane().add(BorderLayout.NORTH, textArea);
+        f.setSize(500, 500);
+        f.setLocation(300,200);
+        final JTextArea textArea = new JTextArea(5, 10);
+        f.getContentPane().add(BorderLayout.NORTH, textArea);
     
     
-    JPanel userCharacter = new JPanel();
-    userCharacter.setSize(300,300);
-    JPanel inputBar = new JPanel();
-    f.getContentPane().add(BorderLayout.CENTER, userCharacter);
-    f.getContentPane().add(BorderLayout.SOUTH, inputBar);
-    final JButton button = new JButton("Click Me");
-    inputBar.add(BorderLayout.WEST, button);
-    final JButton button2 = new JButton("Click Me");
-    inputBar.add(BorderLayout.EAST, button2);
-    userCharacter.setBackground(Color.GRAY);
-    final JButton button3 = new JButton("Click Me");
-    inputBar.add(BorderLayout.EAST, button3);
-    final JButton button4 = new JButton("Click Me");
-    inputBar.add(BorderLayout.EAST, button4);
+        JPanel userCharacter = new JPanel();
+        userCharacter.setSize(300,300);
+        JPanel inputBar = new JPanel();
+        f.getContentPane().add(BorderLayout.CENTER, userCharacter);
+        f.getContentPane().add(BorderLayout.SOUTH, inputBar);
+        final JButton button = new JButton("Click Me");
+        inputBar.add(BorderLayout.WEST, button);
+        final JButton button2 = new JButton("Click Me");
+        inputBar.add(BorderLayout.EAST, button2);
+        userCharacter.setBackground(Color.GRAY);
+        final JButton button3 = new JButton("Click Me");
+        inputBar.add(BorderLayout.EAST, button3);
+        final JButton button4 = new JButton("Click Me");
+        inputBar.add(BorderLayout.EAST, button4);
 
 
-    //Frame
+    //Frame 
+    /* 
     ImageIcon image = new ImageIcon(getClass().getResource("AphexTwinLogo.gif"));
     JLabel displayField = new JLabel(image);
     userCharacter.add(displayField);
     displayField.setVisible(true);
-
+        */
 
     button.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
             textArea.setText(" ");
-            textArea.append(" ONE \n");
+            //textArea.append(" THREE \n");         // <--- Old Command, kept for reference
+            textArea.append(getText(storyProgress, 1));
 
         }
     });
@@ -63,7 +69,7 @@ public class Engine {
         @Override
         public void actionPerformed(ActionEvent e) {
             textArea.setText(" ");
-            textArea.append(" TWO \n");
+            textArea.append(getText(storyProgress, 2));
 
         }
     });
@@ -72,7 +78,7 @@ public class Engine {
         @Override
         public void actionPerformed(ActionEvent e) {
             textArea.setText(" ");
-            textArea.append(" THREE \n");
+            textArea.append(getText(storyProgress, 3));
 
         }
     });
@@ -81,7 +87,7 @@ public class Engine {
         @Override
         public void actionPerformed(ActionEvent e) {
             textArea.setText(" ");
-            textArea.append(" FOUR \n");
+            textArea.append(getText(storyProgress, 4));
 
         }
     });
@@ -89,16 +95,82 @@ public class Engine {
     f.setVisible(true);
 
     } //End Of Engine() Constructor
+
   public static void main(String[] args) {
         Engine start = new Engine();
-    
         } // End of Main
   
   public class GUI {
-
+    
   }
 
+  public String getText(int timeLine, int button){
+    String text = buttonSpecificText(timeLine, button);
+    storyProgress ++;
+    return text;
+  }
 
+  public String buttonSpecificText(int timeLine, int button){
+    String text = "";
+
+    if(button == 1){
+        //Text Responses for button 1
+        switch (timeLine) {
+            case 1:
+                text = "No fuck you";
+              break;
+            case 2:
+                text = "Yeah";
+              break;
+            default:
+              text = "Unsupported Switch";
+          }
+
+    } else if (button == 2){
+        //Text Responses for button 2
+        switch (timeLine) {
+            case 1:
+                text = "Maybe";
+              break;
+            case 2:
+                text = "No";
+              break;
+            default:
+              text = "Unsupported Switch";
+          }
+
+    } else if (button == 3){
+        //Text responses for button 3
+        switch (timeLine) {
+            case 1:
+                text = "perchance";
+              break;
+            case 2:
+                text = "gilgamesh";
+              break;
+            default:
+              text = "Unsupported Switch";
+          }
+
+    } else if (button == 4){
+        //Text responses for button 4
+        switch (timeLine) {
+            case 1:
+                text = "Shutup";
+              break;
+            case 2:
+                text = "you liar";
+              break;
+            default:
+              text = "Unsupported Switch";
+          }
+
+    } else {
+        text = "Unsupported Boolean";
+    }
+
+    return text;
+  }
 
 
 
